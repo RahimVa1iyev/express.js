d748e6b0e6c78cafa9c1c983c946373fad75cee7
rahimvaliyev <rahimvaliyev99@gmail.com>
2023-12-31 00:47:23 +0400

    add-routes

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const {people} = require('./data.js')
-
-const app = express() ; 
+const peopleRouter = require('./routes/people')
+const authRouter = require('./routes/auth')
+const app = express() 
 
 app.use(express.static('./methods-public'))
+
 
 // parse json
 app.use(express.json())
@@ -11,62 +12,11 @@ app.use(express.json())
 // parse form-data
 app.use(express.urlencoded({extended :false}))
 
-app.get('/api/people',(req,res) =>{
-    res.status(200).json({success:true , data : people})
-})
-
-// json
-app.post('/api/people',(req,res)=>{
-    const {name} = req.body
-    if(!name){
-        return res.status(400).json({success :false , msj: 'please provide name value'})
-    }
-
-    res.status(201).json({success:true,person:name})
-})
-
 // form-data
-app.post('/login', (req,res) => {
-   const {name} = req.body
-   if(!name){
-    return res.status(401).send("Unauthorized")
-   }
-   res.status(200).send(`<h1>Hello ${name}</h1>`)
-})
 
-app.put('/api/people/:id',(req,res) => {
-    const {id} = req.params
-    const {name} =req.body
-    const person = people.find(person => person.id === Number(id))
-    if(!person){
-        return res
-        .status(404)
-        .json(`Person not found by id ${id}`)
-    }
+app.use('/login',authRouter)
 
-    const updatedPeople = people.map(person =>{
-        if(person.id === Number(id)){
-            person.name = name
-        }
-        return person
-    })
-
-    res.status(200).json({success: true , data : updatedPeople})
-})
-
-app.delete('/api/people/:id' , (req,res) => {
-    const {id} = req.params
-    const person = people.find(person => person.id === Number(id))
-    if(!person){
-        return res
-        .status(404)
-        .json(`Person not found by id ${id}`)
-    }
-
-    const deletedPerson = people.filter(person => person.id !== Number(id))
-    return res.status(200).json({success:true , data : deletedPerson})
-
-})
+app.use('/api/people',peopleRouter)
 
 
 app.listen(5000,(req,res)=>{
