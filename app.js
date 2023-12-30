dbfefdb2e9c560f906263ebc7f7a3fd60ae48537
rahimvaliyev <rahimvaliyev99@gmail.com>
2023-12-30 17:57:17 +0400

    basic-json

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express')
-const path = require('path')
-const app = express();
+const {products} = require('./data.js')
+const app = express()
 
-app.use(express.static('./public'))
-
-app.get('/',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+app.get('/' ,(req,res) => {
+    res.send("<h1>Home Page</h1> <a href='/api/products' >products</a>")
 })
 
-app.all('*', (req,res)=>{
-    res.status(404).send('resource not found')
+app.get('/api/products', (req,res) =>{
+    const newProducts = products.map(product => {
+        const {id,name,image} = product
+        return {id,name,image}
+    })
+    res.json(newProducts)
 })
 
-app.listen(5000,()=>{
-    console.log('listen port 5000..');
+app.get('/api/products/:productID', (req,res)=>{
+    const {productID} = req.params
+    console.log(productID);
+
+    const singleProduct = products.find((product)=> product.id === Number(productID) )
+
+    if(!singleProduct) return res.status(404).send("<h1>Products not found</h1>")
+    
+    return res.json(singleProduct)
+})
+
+app.listen(5000,(req,res)=>{
+    console.log('Server listening port 5000...');
 })
